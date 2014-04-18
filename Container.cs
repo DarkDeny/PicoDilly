e009d4394b9753f23e73bdbbc256dd8697778dc5
Dmitry Denisov <darkdeny@gmail.com>
2014-04-18 17:46:50 +0400

    Initial commit

---
+++ Container.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace PicoDilly {
+    public class Container : IContainer {
+        private readonly Dictionary<Type, object> _dict;
+        private Type _currentConfigurableItem;
+
+        public Container() {
+            _dict = new Dictionary<Type, Object>();
+        }
+
+        public T GetInstance<T>() {
+            return (T)GetInstance(typeof(T));
+        }
+
+        private object[] GetConstructorArgs(ConstructorInfo ctor) {
+            var list = new List<object>();
+            foreach (var parameterInfo in ctor.GetParameters()) {
+                var dependencyType = parameterInfo.ParameterType;
+                var dependency = GetInstance(dependencyType);
+                list.Add(dependency);
+            }
+
+            return list.ToArray();
+        }
+
+        private object GetInstance(Type dependencyType) {
+            if (_dict.ContainsKey(dependencyType)) {
+                return _dict[dependencyType];
+            }
+
+            object[] ctorArgs;
+            var constructor = dependencyType.GetConstructor(Type.EmptyTypes);
+            if (null != constructor) {
+                ctorArgs = new object[] { };
+            } else {
+                constructor = dependencyType.GetConstructors().First();
+                ctorArgs = GetConstructorArgs(constructor);
+            }
+
+            var result = constructor.Invoke(ctorArgs);
+            return result;
+        }
+
+        public void Configure(Action<IContainer> configureAction) {
+            configureAction(this);
+        }
+
+        public IContainer For<T>() {
+            _currentConfigurableItem = typeof(T);
+            return this;
+        }
+
+        public IContainer Use<T>(T instance) {
+            if (null == _currentConfigurableItem) {
+                throw new InvalidOperationException();
+            }
+            _dict.Add(_currentConfigurableItem, instance);
+            _currentConfigurableItem = null;
+            return this;
+        }
+    }
+}
---
+++ IContainer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PicoDilly {
+    public interface IContainer {
+        T GetInstance<T>();
+        void Configure(Action<IContainer> configureAction);
+        IContainer For<T>();
+        IContainer Use<T>(T instance);
+    }
+}
